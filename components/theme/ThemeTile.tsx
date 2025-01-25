228d8498bebf5f519ec34a133cab47231437f2a0
Dev Mobile ICT <devmobile@ict-consulting.tec>
2025-01-25 14:49:06 +0100

    first commit

---
+++ components/theme/ThemeTile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LightThemeTile = () => {
+  return (
+      <div className="h-[40px] rounded-[29px] p-y-2 p-x-4 bg-foreground space-x-4">
+          <div className="w-6 h-5 round-full">
+          </div>
+      </div>
+  );
+};
+
+export default LightThemeTile;
